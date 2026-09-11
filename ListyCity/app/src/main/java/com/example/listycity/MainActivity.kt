78689b5fdae024ac3ee6b555da71cf9fe08d9c59
alext-24 <alextang.prof@gmail.com>
2026-09-10 18:27:32 -0600

    Some changes and comments

--- ListyCity/app/src/main/java/com/example/listycity/MainActivity.kt
+++ ListyCity/app/src/main/java/com/example/listycity/MainActivity.kt
@@ -36,62 +36,82 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     fun CityListScreen(
+        // this screen's purpose is to display cities
         cities: List<String>,
         onAddCity: (String) -> Unit,
         onRemoveCity: (String) -> Unit,
         modifier: Modifier = Modifier
     ) {
-        var newCityName by remember {mutableStateOf("")}
+        var newCityName by remember {mutableStateOf("")} // this variable is our text field in the text box
         var selectedCity by remember {mutableStateOf<String?>(null)}
         // <String?> means that selectedCity can be of type String or null; (null) means that the variable is initialized as null (because we don't have a city selected at the start)
+        var userAddingCity by remember {mutableStateOf(false)} // state initializes as False; this variable tracks whether the user has tapped "Add City"
 
         Column(modifier = modifier.fillMaxSize()) {
-            Row(modifier = modifier.padding(all = 12.dp)) {
-                OutlinedTextField(
-                    value = newCityName,
-                    onValueChange = {newCityName = it},
-                    label = {Text("City name")},
-                    modifier = Modifier.weight(1f)
-                )
 
-                Spacer(modifier = Modifier.width(8.dp))
+            // this column is how we're going to display the list of cities (and also the buttons)
+            LazyColumn(modifier = modifier.weight(1f)) {
+                // this LazyColumn is the list of cities
+                items(cities) { city ->
+                    CityRow(
+                        // a city is displayed in its own row
+                        city = city,
+                        selected = (selectedCity == city), // selected is the boolean value of this expression
+                        onClick = { // when the row is tapped, the selectedCity value becomes that "city"
+                            selectedCity = city
+                        }
+                    )
+                }
+            }
 
-                // add city button functionality
+            if (userAddingCity) {
+                // if user has tapped "Add City"
+                Row(modifier = modifier.padding(12.dp)) {
+                    OutlinedTextField(
+                        // this is the text box element that we're using to user input
+                        value = newCityName,
+                        onValueChange = { newCityName = it },
+                        label = { Text("City name") },
+                        modifier = Modifier.weight(1f)
+                    )
+
+                    Spacer(modifier = Modifier.width(8.dp))
+
+                    // add city button functionality
+                    Button(
+                        onClick = {
+                            if (newCityName.isNotBlank()) {
+                                onAddCity(newCityName)
+                                newCityName = ""
+                                userAddingCity = false
+                            }
+                        }
+                    ) {
+                        Text("Confirm")
+                    }
+                }
+            } else {
                 Button(
                     onClick = {
-                        if (newCityName.isNotBlank()) {
-                            onAddCity(newCityName)
-                            newCityName = ""
-                        }
+                        userAddingCity = true
                     }
                 ) {
                     Text("Add city")
-                }
-
-                // remove city button functionality
-                Button(
-                    onClick = {
-                        selectedCity?.let {city ->
-                            onRemoveCity(city)
-                            selectedCity = null
-                        }
+            }
+            // remove city button functionality
+            Button(
+                onClick = {
+                    val city = selectedCity
+                    if (city != null) {
+                        // when the button is tapped, we check for whether city is null or not, if not, then we (probably) tapped a city and we remove it
+                        onRemoveCity(city)
+                        selectedCity = null
                     }
-                ) {
-                    Text("Remove city")
                 }
+            ) {
+                Text("Remove city")
             }
-
-            // displays city in a row element on screen
-            LazyColumn(modifier = modifier.fillMaxSize()) {
-                items(cities) { city ->
-                    CityRow(
-                        city = city,
-                        selected = selectedCity == city,
-                        onClick = {
-                            selectedCity = city
-                        })
                 }
-            }
         }
     }
 
@@ -106,7 +126,7 @@ class MainActivity : ComponentActivity() {
             fontSize = 28.sp,
             modifier = Modifier
                 .fillMaxWidth()
-                .selectable(
+                .selectable( // allows each CityRow instance to be selectable, and also has flags for tracking selected and onClick
                     selected = selected,
                     onClick = onClick
                 )
--- README.md
+++ README.md
@@ -5,7 +5,9 @@
 - **CCID:** `atang1`
 
 ## References and Resources
-- something about Kotlin selectable here
+- I learned how to use .selectable() from: https://medium.com/@elementalistbtg/selectable-and-clickable-items-in-a-list-in-jetpack-compose-desktop-27ce5fbd9354
+- Further documentation about .selectable() from the official android dev site: https://developer.android.com/develop/ui/compose/modifiers-list
+- This kotlinlang page talks about using nullable types: https://kotlinlang.org/docs/null-safety.html?utm_source=chatgpt.com#nullable-types-and-non-nullable-types, used for \<String?\> to allow nulls and strings in the same variable
 
 ## Verbal Collaboration
 List students' names and CCIDs here, or simply put `N/A` if not applicable. 
